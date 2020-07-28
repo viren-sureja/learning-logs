71259164bf64faa3f6fb2d44c5ed1d7214638cea
viren <virensureja3@gmail.com>
2020-07-28 23:59:31 +0530

    over

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect,Http404
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
@@ -23,7 +23,7 @@ def topics(req):
 @login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     # Make sure the topic belongs to the current user.
     if topic.owner != request.user:
         raise Http404
